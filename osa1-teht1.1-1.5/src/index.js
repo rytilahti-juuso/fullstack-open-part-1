3c78c4dd0311ddb3ecd8fe69ecef5a9c140ea248
rytilahti-juuso <rytilahti.juuso@gmail.com>
2018-08-02 15:12:18 +0300

    1.2 tehty

--- osa1-teht1.1-1.5/src/index.js
+++ osa1-teht1.1-1.5/src/index.js
@@ -9,13 +9,29 @@ const Otsikko = (props) => {
   )
 }
 
-const Sisalto = (props) => {
+const Osa = (props) => {
   return (
     <div>
       <p>{props.osa} {props.tehtavia}</p>
     </div>
   )
 }
+
+
+const Sisalto = (props) => {
+
+
+  return (
+    <div>
+    <Osa osa={props.osa1}  tehtavia={props.tehtavia1}/>
+    <Osa osa={props.osa2}  tehtavia={props.tehtavia2}/>
+    <Osa osa={props.osa3}  tehtavia={props.tehtavia3}/>
+
+
+    </div>
+  )
+}
+
 
 const Yhteensa = (props) => {
   return (
@@ -37,9 +53,7 @@ const App = () => {
   return (
     <div>
       <Otsikko kurssi= {kurssi} />
-      <Sisalto  osa= {osa1} tehtavia={tehtavia1}/>
-      <Sisalto  osa= {osa2} tehtavia={tehtavia2}/>
-      <Sisalto  osa= {osa3} tehtavia={tehtavia3}/>
+      <Sisalto osa1={osa1} tehtavia1={tehtavia1} osa2={osa2} tehtavia2={tehtavia2} osa3={osa3} tehtavia3={tehtavia3}  />
       <Yhteensa yhteensa = {tehtavia1 + tehtavia2 + tehtavia3} />
     </div>
   )
