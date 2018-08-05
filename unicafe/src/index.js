d16e704027e8da2d3c74e972d80787c6dc8db5b4
rytilahti-juuso <rytilahti.juuso@gmail.com>
2018-08-05 19:17:09 +0300

    unicafe osa 1 ratkaistu

--- unicafe/src/index.js
+++ unicafe/src/index.js
@@ -1,10 +1,50 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const App = () => (
-  <div>
-    <p>Hello world</p>
-  </div>
-)
+
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      hyva: 0,
+      neutraali: 0,
+      huono: 0
+    }
+  }
+
+  kasvataYhdella = (nappi) =>{
+    return () => {
+      if(nappi === 'hyva'){
+        this.setState({ hyva: this.state.hyva + 1 })
+      }
+      else if(nappi === 'neutraali'){
+        this.setState({ neutraali: this.state.neutraali + 1 })
+      }
+      else if(nappi === 'huono'){
+        this.setState({ huono: this.state.huono + 1 })
+      }
+    }
+  }
+
+
+
+  render() {
+    return (
+      <div>
+        <h1>Anna palautetta</h1>
+        <button onClick ={ this.kasvataYhdella('hyva')} >Hyvä</button>
+        <button onClick ={ this.kasvataYhdella('neutraali')}>Neutraali</button>
+        <button onClick ={ this.kasvataYhdella('huono')}>Huono</button>
+        <h1> Statistiikka</h1>
+        <p>hyvä {this.state.hyva}</p>
+        <p>neutraali {this.state.neutraali}</p>
+        <p>huono {this.state.huono}</p>
+      </div>
+    )
+  }
+}
+
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'))
