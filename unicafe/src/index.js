b241c949e7203223245c33521e5661de804f8c79
rytilahti-juuso <rytilahti.juuso@gmail.com>
2018-08-07 10:19:36 +0300

    unicafe osa3 osa tehty
    Button komponentti toimii täysin

--- unicafe/src/index.js
+++ unicafe/src/index.js
@@ -14,6 +14,26 @@ const Statistics = (props) => {
   )
 }
 
+const Statistic = (props) => {
+  return(
+    <div>
+      <h1> Statistiikka</h1>
+      <p>hyvä {props.hyva}</p>
+      <p>neutraali {props.neutraali}</p>
+      <p>huono {props.huono}</p>
+      <p>keskiarvo</p>
+      <p>positiivisia</p>
+    </div>
+  )
+}
+
+const Button = (props) => {
+  return(
+    
+<button onClick ={ props.kasvata} >{props.teksti}</button>
+
+  )
+}
 
 
 class App extends React.Component {
@@ -42,21 +62,32 @@ class App extends React.Component {
 
 
 
+
   render() {
 
     return (
       <div>
-        <h1>Anna palautetta</h1>
-        <button onClick ={ this.kasvataYhdella('hyva')} >Hyvä</button>
-        <button onClick ={ this.kasvataYhdella('neutraali')}>Neutraali</button>
-        <button onClick ={ this.kasvataYhdella('huono')}>Huono</button>
+        <h1>Anna Palautetta</h1>
+
+        <Button
+        teksti="Hyvä"
+        kasvata= {this.kasvataYhdella("hyva")}
+        />
+        <Button
+        teksti="Neutraali"
+        kasvata= {this.kasvataYhdella("neutraali")}
+        />
+        <Button
+        teksti="Huono"
+        kasvata= {this.kasvataYhdella("huono")}
+        />
         <Statistics />
         <h1> Statistiikka</h1>
         <p>hyvä {this.state.hyva}</p>
         <p>neutraali {this.state.neutraali}</p>
         <p>huono {this.state.huono}</p>
         <p>keskiarvo {(this.state.hyva+(this.state.huono*(-1)))/(this.state.huono+ this.state.hyva + this.state.neutraali)}</p>
-        <p>positiivisia {100*(this.state.hyva / (this.state.huono+ this.state.hyva + this.state.neutraali))}.toFixed(2)%</p>
+        <p>positiivisia {100*(this.state.hyva / (this.state.huono+ this.state.hyva + this.state.neutraali))}%</p>
       </div>
     )
   }
